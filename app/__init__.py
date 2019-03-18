a6f404da24a628560ae717461759f1c02a0bc6d4
alexnro <alejandro.navarro@ETOOL.TECH>
2019-03-18 10:52:50 +0100

    Added text translated to spanish

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 
 
 app = Flask(__name__)
@@ -18,6 +18,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -50,9 +51,10 @@ if not app.debug:
         app.logger.setLevel(logging.INFO)
         app.logger.info('Microblog startup')
 
-from app import routes, models, errors
-
 
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+from app import routes, models, errors
