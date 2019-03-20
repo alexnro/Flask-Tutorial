a7ddc69f1d0052c8bc8f4b7cafb960ac38040519
alexnro <alejandro.navarro@ETOOL.TECH>
2019-03-20 12:59:29 +0100

    Text search implemented but not working correctly

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,3 +1,4 @@
+
 from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request, g, \
     current_app
@@ -15,7 +16,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
-        g.search_from = SearchForm()
+        g.search_form = SearchForm()
     g.locale = str(get_locale())
 
 
