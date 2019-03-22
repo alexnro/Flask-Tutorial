013c3dac68b5e56e604b403dc997007fb61eb3a0
alexnro <alejandro.navarro@ETOOL.TECH>
2019-03-22 12:54:16 +0100

    Add notifications to private messages

--- app/models.py
+++ app/models.py
@@ -131,6 +131,12 @@ class User(UserMixin, db.Model):
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.commit()
+        return n
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
