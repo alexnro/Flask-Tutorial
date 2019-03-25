933f1cf2f26aaf9165ed9b56877dacf32cabbd19
alexnro <alejandro.navarro@ETOOL.TECH>
2019-03-25 13:54:28 +0100

    Adding api configuration

--- app/api/errors.py
+++ app/api/errors.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from werkzeug.http import HTTP_STATUS_CODES
 
 
-def error_responde(status_code, message=None):
+def error_response(status_code, message=None):
     payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknwon error')}
     if message:
         payload['message'] = message
