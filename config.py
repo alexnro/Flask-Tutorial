bfe85c1c7137f637538549575b2b2a719b4aa8a6
alexnro <alejandro.navarro@ETOOL.TECH>
2019-04-03 13:06:14 +0200

    Deleted some commented lines

--- config.py
+++ config.py
@@ -15,8 +15,6 @@ class Config(object):
     USERS_COLLECTION = CLIENT.blogUsers
     FOLLOWERS_COLLECTION = CLIENT.followers
     # config["MONGO_URI"] = "mongodb://localhost:27017/microblog"
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
